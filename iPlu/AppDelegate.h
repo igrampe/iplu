b0700feaef9a9f8a08d754e728556e6a2d49e01a
Sema Belokovsky <igrampe@gmail.com>
2012-07-28 18:04:10 +0600

    Initial Commit

---
+++ iPlu/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iPlu
+//
+//  Created by Semen Belokovsky on 28.07.12.
+//  Copyright (c) 2012 Semen Belokovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iPlu/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iPlu
+//
+//  Created by Semen Belokovsky on 28.07.12.
+//  Copyright (c) 2012 Semen Belokovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
